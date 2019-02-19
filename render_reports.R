17cc91fc254e041b547d07af3e56aca2d85d1bdb
Marc Melliger <marc@melliger.com>
2019-02-19 09:49:10 +0100

    Clean up code and work on final document format

--- render_reports.R
+++ render_reports.R
@@ -2,16 +2,20 @@
 filename <- paste("analysis_PV",Sys.Date(),sep = "_")
 report_currency <- "USD"
 
+
+#BNEF marketshares
+rmarkdown::render("analysis_learningrates_corrections.Rmd", output_format = "pdf_document", 
+                  output_file = paste0(filename, "_BNEF_", report_currency, ".pdf"), output_dir = "output", 
+                  params = list(
+                    delta = "BNEF",
+                    use_kable: "yes",
+                    lead_currency = report_currency))
+
+
 #IRENA marketshares
 rmarkdown::render("analysis_learningrates_corrections.Rmd", output_format = "pdf_document", 
                   output_file = paste0(filename, "_IRENA_", report_currency, ".pdf"), output_dir = "output", 
                   params = list(
-                    lead_currency = report_currency))
+                    lead_currency = report_currency,
+                    use_kable: "yes"))
 
-
-#BNEF marketshares
-rmarkdown::render("analysis_learningrates_corrections.Rmd", output_format = "pdf_document", 
-                    output_file = paste0(filename, "_BNEF_", report_currency, ".pdf"), output_dir = "output", 
-                    params = list(
-                      delta = "BNEF",
-                      lead_currency = report_currency))
