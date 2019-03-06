5eab7d8344916cf362796c83b97e61d4e05b2c8c
Marc Melliger <marc@melliger.com>
2019-03-06 13:20:55 +0100

    New render

--- render_reports.R
+++ render_reports.R
@@ -11,6 +11,14 @@ rmarkdown::render("analysis_learningrates_corrections.Rmd", output_format = "pdf
                     x_global_for_cumsum = "BNEF_relevant",
                     use_kable = TRUE,
                     lead_currency = report_currency))
+# doc
+rmarkdown::render("analysis_learningrates_corrections.Rmd", output_format = "word_document", 
+                  output_file = paste0(filename, "_BNEF_", report_currency, ".docx"), output_dir = "output", 
+                  params = list(
+                    delta = "BNEF",
+                    x_global_for_cumsum = "BNEF_relevant",
+                    use_kable = TRUE,
+                    lead_currency = report_currency))
 
 
 #IRENA marketshares
@@ -36,8 +44,8 @@ rmarkdown::render("analysis_learningrates_corrections.Rmd", output_format = "pdf
 # Set to the same norm ----------------------------------------------------
 
 #BNEF marketshares
-rmarkdown::render("analysis_learningrates_corrections.Rmd", output_format = "pdf_document", 
-                  output_file = paste0(filename, "_BNEF_samestart_", report_currency, ".pdf"), output_dir = "output", 
+rmarkdown::render("analysis_learningrates_corrections.Rmd", output_format = "word_document", 
+                  output_file = paste0(filename, "_BNEF_same_startcapacity_", report_currency, ".docx"), output_dir = "output", 
                   params = list(
                     delta = "BNEF",
                     x_global_for_cumsum = "BNEF_relevant",
@@ -47,8 +55,8 @@ rmarkdown::render("analysis_learningrates_corrections.Rmd", output_format = "pdf
 
 
 #IRENA marketshares
-rmarkdown::render("analysis_learningrates_corrections.Rmd", output_format = "pdf_document", 
-                  output_file = paste0(filename, "_IRENA_samestart_", report_currency, ".pdf"), output_dir = "output", 
+rmarkdown::render("analysis_learningrates_corrections.Rmd", output_format = "word_document", 
+                  output_file = paste0(filename, "_IRENA_same_startcapacity_", report_currency, ".docx"), output_dir = "output", 
                   params = list(
                     delta = "IRENA",
                     x_global_for_cumsum = "IRENA",
