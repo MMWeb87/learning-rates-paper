2becfb73c564e588926eeb3a628fb137ea5ea653
Marc Melliger <9552031+MMWeb87@users.noreply.github.com>
2019-09-17 11:01:43 +0200

    Removed method 1, adjusted plots

--- render_reports.R
+++ render_reports.R
@@ -6,14 +6,14 @@ for(report_currency in report_currencies){
   # Use these results for the main paper. It's based on the IRENA capacity development and marketshares.
   # The capacity in 2006 is not changed.
   
-  rmarkdown::render("analysis_learningrates_corrections.Rmd", output_format = "word_document", 
+  rmarkdown::render("calculations_and_data.Rmd", output_format = "word_document", 
                     output_file = paste0(filename, "_", report_currency, "_real.docx"), output_dir = "output/reports/", 
                     params = list(
                       deflate = TRUE,
                       base_currency = report_currency,
                       use_kable = TRUE))
   
-  rmarkdown::render("analysis_learningrates_corrections.Rmd", output_format = "word_document", 
+  rmarkdown::render("calculations_and_data.Rmd", output_format = "word_document", 
                     output_file = paste0(filename, "_", report_currency, "_nominal.docx"), output_dir = "output/reports/", 
                     params = list(
                       deflate = FALSE,
@@ -22,14 +22,10 @@ for(report_currency in report_currencies){
 }
 
 # files
-rmarkdown::render("analysis_learningrates_corrections.Rmd", output_format = "pdf_document", 
-                  output_file = paste0(filename, ".pdf"), output_dir = "output/reports_files/", 
-                  params = list(use_kable = TRUE))
-
-rmarkdown::render("analysis_learningrates_corrections.Rmd", output_format = "html_document", 
-                  output_file = paste0(filename, ".html"), output_dir = "output/reports_files/", 
+rmarkdown::render("calculations_and_data.Rmd", output_format = "pdf_document",
+                  output_file = paste0(filename, ".pdf"), output_dir = "output/reports_files/",
                   params = list(use_kable = TRUE))
 
 
-rmarkdown::render("analysis_results.Rmd", output_format = "pdf_document", 
+rmarkdown::render("plots_and_tables.Rmd", output_format = "pdf_document", 
                   output_file = paste0("all_results.pdf"), output_dir = "output/reports_files/")
